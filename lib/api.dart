209cf069f23b863c0de01082302fd4811f1b64b1
HastagKc <chaudharykshittiz950@gmail.com>
2023-05-10 14:58:25 +0545

    api class is added

--- lib/api.dart
+++ lib/api.dart
@@ -1,6 +1,8 @@
+//! creating api class with static fields
+
 class Api {
-  static const baseUrl = ' https://api.themoviedb.org/3';
+  static const baseUrl = "https://api.themoviedb.org/3";
   static const getPopular = '$baseUrl/movie/popular';
   static const getTopRated = '$baseUrl/movie/top_rated';
-  static const getUpcoming = '$baseUrl/movie/upcoming';
+  static const getUpcomming = '$baseUrl/movie/upcomming';
 }
